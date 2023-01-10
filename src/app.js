299a5facb51bd24324cc367dcc38b951d4b2e680
anthonyeunerd <anthony.freitas@eunerd.com>
2023-01-10 14:00:10 -0400

    fix: cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const { routes } = require('./routes/index.js');
-
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+        credentials: true,
+    })
+);
 app.use(routes);
 
 module.exports = { app };
